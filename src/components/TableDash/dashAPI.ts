d7a5b60235b6ef99c71f0b91d0e2bde836bbd0bf
MinaSameh1 <minasameh1@gmail.com>
2022-06-22 09:38:08 +0200

    added  count

--- src/components/TableDash/dashAPI.ts
+++ src/components/TableDash/dashAPI.ts
@@ -3,6 +3,7 @@ import { data } from './types'
 
 export async function getData(page: number = 1, sort: string = 'id') {
   const res = await axiosInstance.get(`/schools?_page=${page}&_sort=${sort}`)
+  const total = res.headers['x-total-count']
   console.log(res.data)
-  return res.data as data[]
+  return { data: res.data as data[], count: Number(total) }
 }
